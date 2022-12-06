e18274d1ea1079c0b9492cb9c7b0f033cc7a4f8c
sayray303 <andi.prayogi30@gmail.com>
2022-12-06 14:09:34 +0700

    revi4

--- resources/views/masyarakat/home.blade.php
+++ resources/views/masyarakat/home.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
     <div class="container-fluid">
-        <h1 class="text-black-50">Kamu adalah masyarakat</h1>
+        <img class="img-fluid rounded" src="img/logo.png" alt="" width="300">
+        <h1 class="text-black-50">Selamat Datang di Halaman Pengaduan di LI-TPK Palembang</h1>
+        <h1 class="text-black-50">Kamu Berhasil Login dan Silahkan Buat Aduan</h1>
     </div>
 @endsection
--- resources/views/pimpinan/home.blade.php
+++ resources/views/pimpinan/home.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
-        <h1 class="text-black-50">Kamu adalah pimpinan</h1>
-    </div>
+<div class="container-fluid">
+    <img class="img-fluid rounded" src="img/logo.png" alt="" width="300">
+    <h1 class="text-black-50">Selamat Datang di Halaman Pengaduan di LI-TPK Palembang</h1>
+    <h1 class="text-black-50">Silahkan Lihat laporan di menu Pengaduan</h1>
+</div>
 @endsection
