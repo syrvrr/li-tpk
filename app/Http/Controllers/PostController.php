241003762cad43b52710b49f57b6cbd995037c3b
sayray303 <andi.prayogi30@gmail.com>
2022-12-06 11:31:08 +0700

    updateview

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\models\pengaduan;
 
 class PostController extends Controller
 {
-    public function cari(Request $request)
+    public function search(Request $request)
     {
-        if($request->has('cari')){
-            $pengaduan = Pengaduan::where('kode','LIKE','%');
-
+        if($request->has('search')){
+            $pengaduan = Pengaduan::where('kode','LIKE','%' . $request->search.'%');
+        }else{
+            $pengaduan = Pengaduan::paginate(5);
         }
+        return view('cari')
     }
 }
